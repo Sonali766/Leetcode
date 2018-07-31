d65c3e13070ecd055c6d6d246e53f22181f19654
sonali766 <sonali766@gmail.com>
2018-07-30 22:53:48 -0700

    commit fixed

--- Array/1D_array/pivot-index.py
+++ Array/1D_array/pivot-index.py
@@ -3,8 +3,13 @@ class Array:
 		sum_l = 0
 		sum_R = sum(nums)
 		for i in range(len(nums)):
-			sum_R1 -= nums[i]
+			sum_R -= nums[i]
 			if sum_l == sum_R:
 				return i
 			sum_l += nums[i]
 		return -1
+
+n = [1,2,3,5,6]
+a = Array()
+print a.pivotindex(n)
+
